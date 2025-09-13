3da5ab200f41636c45c639b3376d7c66e231de10
emannn077 <emanrauf779@gmail.com>
2025-09-13 22:56:40 +0300

    added script.js and ids

---
+++ script.js
@@ -0,0 +1,32 @@
+//Global Variables
+const levelEasy = document.querySelector("#easy")
+const levelMedium = document.querySelector("#medium")
+const levelDifficult = document.querySelector("#difficult")
+
+//Function
+
+const nextPage = (level) => {
+  window.location.href = `${level}.html`
+}
+const handleClick = (gameLevel) => {
+  if (gameLevel === "easy") {
+    nextPage("easy")
+  } else if (gameLevel === "medium") {
+    nextPage("medium")
+  } else if (gameLevel === "difficult") {
+    nextPage("difficult")
+  } else {
+    console.error("Error", gameLevel)
+  }
+}
+//event Listeners
+
+levelEasy.addEventListener("click", () => {
+  handleClick("easy")
+})
+levelMedium.addEventListener("click", () => {
+  handleClick("medium")
+})
+levelDifficult.addEventListener("click", () => {
+  handleClick("difficult")
+})
