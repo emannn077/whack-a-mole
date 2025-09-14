249620c9b66e595daa3d6aeb2a358f3bfb0c964d
emannn077 <emanrauf779@gmail.com>
2025-09-15 00:18:14 +0300

    score and timer added in easy.js with some css adjustments.

--- easy.js
+++ easy.js
@@ -12,20 +12,55 @@ const timerSection = document.getElementById("Timer")
 const moles = document.querySelectorAll(".mole")
 
 let moleTimer
+let countdownTimer
+
+const endGame = () => {
+  clearInterval(moleTimer)
+  clearInterval(countdownTimer)
+
+  holes.forEach((sq) => {
+    sq.textContent = ""
+  })
+
+  alert(`Game over ! your score is ${score}`)
+}
+const countDown = () => {
+  timeLeft-- //its like timeLeft= timeLeft-1
+  timerSection.textContent = `Time: ${timeLeft}s`
+
+  if (timeLeft <= 0) {
+    endGame() //it will stop the game when timer is up
+  }
+}
 
 const start = () => {
   moleTimer = setInterval(showMole, 1500)
+
+  countdownTimer = setInterval(countDown, 1000) //added start countdown
+
+  score = 0
+  timeLeft = gameDuration
+  scoreSection.textContent = `Score: ${score}`
+  timerSection.textContent = `Time: ${timeLeft}s`
 }
 
 const showMole = () => {
   holes.forEach((sq) => {
     sq.textContent = ""
+    sq.onclick = null
   })
 
   let randomSq = Math.floor(Math.random() * NumOfMole)
   let sqIndex = holes[randomSq]
 
   sqIndex.innerText = "mole"
+  // this code is for the index of the hole. its click event losterne that will be for he mole hole
+  sqIndex.onclick = () => {
+    score++
+    scoreSection.textContent = `Score: ${score}`
+    sqIndex.textContent = ""
+    sqIndex.onclick = null // this will remove the old event listener so that it will not the empty hole as score.
+  }
 
   // moleTimeFinish = setTimeout(showMole, activeMole)
 }
