6fbb5d16f0c7776fd9ee14401092ced3a6ce6903
emannn077 <emanrauf779@gmail.com>
2025-09-16 15:23:51 +0300

    added the bounce animation and some css

--- easy.js
+++ easy.js
@@ -12,11 +12,12 @@ const timerSection = document.getElementById("Timer")
 const moles = document.querySelectorAll(".mole")
 const startBtn = document.querySelector("#startBtn")
 const cursor = document.querySelector(".cursor img")
-
-//i made whack animation using the image and added the function by using a reference/guidance through a youtube vidoes for understanding the concept of it,on how to make whack on cursor and how to add whacking animation in it
-
 let moleTimer
 let countdownTimer
+const finalScore = document.querySelector("#finalScore")
+let bestScore = localStorage.getItem("bestScore") || 0
+const bestScoreSection = document.querySelector("#bestScore")
+bestScoreSection.textContent = bestScore
 
 //i used array to store the value/score of each image . i find it MVP
 const differentmoles = [
@@ -24,6 +25,8 @@ const differentmoles = [
   { src: "/Jerry.jpg", points: 5 }, //it will increas score by 5 points
   { src: "/download.jpg", points: -10 }, //it will decrease score by 10 points.
 ]
+
+//for bestScore
 
 const endGame = () => {
   clearInterval(moleTimer)
@@ -33,7 +36,13 @@ const endGame = () => {
     sq.innerHTML = ""
   })
 
-  alert(`Game over ! your score is ${score}`)
+  if (score > bestScore) {
+    bestScore = score
+    localStorage.setItem("bestScore", bestScore)
+    bestScoreSection.textContent = bestScore
+  }
+
+  finalScore.textContent = `Timer is up! Your final score is ${score}` ///this is to show the final score msg
 }
 const countDown = () => {
   timeLeft-- //its like timeLeft= timeLeft-1
@@ -45,6 +54,7 @@ const countDown = () => {
 }
 
 const start = () => {
+  finalScore.textContent = "" //this code will clear previous final score msg
   moleTimer = setInterval(showMole, 1500)
 
   countdownTimer = setInterval(countDown, 1000) //added start countdown
@@ -54,6 +64,8 @@ const start = () => {
   scoreSection.textContent = `Score: ${score}`
   timerSection.textContent = `Time: ${timeLeft}s`
 }
+
+//i made whack animation using the image and added the function by using a reference/guidance through a youtube vidoes for understanding the concept of it,on how to make whack on cursor and how to add whacking animation in it
 window.addEventListener("mousemove", (e) => {
   cursor.style.top = e.pageY + "px"
   cursor.style.left = e.pageX + "px"
@@ -79,13 +91,14 @@ const showMole = () => {
   const randomMole =
     differentmoles[Math.floor(Math.random() * differentmoles.length)]
 
-  // sqIndex.innerText = "mole"
-
   const moleImg = document.createElement("img")
   moleImg.src = randomMole.src
   moleImg.classList.add("moleImg")
   moleImg.style.width = "80px"
   moleImg.style.cursor = "pointer"
+
+  //add bounce effect when mole comes
+  moleImg.classList.add("bounce-once")
 
   //this logic is for if i click on the image mole it will give scores accroding to it
   moleImg.onclick = () => {
@@ -93,8 +106,6 @@ const showMole = () => {
     scoreSection.textContent = `Score: ${score}`
     sqIndex.innerHTML = ""
   }
-
-  // moleTimeFinish = setTimeout(showMole, activeMole)
 
   sqIndex.appendChild(moleImg)
 }
